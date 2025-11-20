5959b84bb7b1152f93a757fe7371ddc4cfe1eb0e
JKjiwon <jwkim.oa@gmail.com>
2025-11-21 02:19:50 +0900

    REFACTOR: LINT 적용

--- src/main/java/com/athenhub/productservice/product/domain/ProductVariant.java
+++ src/main/java/com/athenhub/productservice/product/domain/ProductVariant.java
@@ -5,7 +5,13 @@ import com.athenhub.productservice.product.domain.dto.ProductVariantCreateReques
 import com.athenhub.productservice.product.domain.vo.ProductColor;
 import com.athenhub.productservice.product.domain.vo.ProductSize;
 import com.athenhub.productservice.product.domain.vo.ProductVariantId;
-import jakarta.persistence.*;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
