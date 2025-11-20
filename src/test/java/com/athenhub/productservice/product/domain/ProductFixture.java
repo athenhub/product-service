b7edf871a67c0fc513eea07ef50a112e244b5ac6
JKjiwon <jwkim.oa@gmail.com>
2025-11-20 16:36:42 +0900

    TEST: Product 테스트
    See also: #1

---
+++ src/test/java/com/athenhub/productservice/product/domain/ProductFixture.java
@@ -0,0 +1,40 @@
+package com.athenhub.productservice.product.domain;
+
+import com.athenhub.productservice.product.domain.dto.ProductCreateRequest;
+import com.athenhub.productservice.product.domain.dto.ProductVariantCreateRequest;
+import java.util.UUID;
+
+public class ProductFixture {
+
+  public static ProductCreateRequest productCreateRequest(ProductType type) {
+    return new ProductCreateRequest(UUID.randomUUID(), UUID.randomUUID(), 10_000L, type);
+  }
+
+    public static Product create(
+            ProductCreateRequest productCreateRequest,
+            ProductVariantCreateRequest... productVariantCreateRequest) {
+        Product product = Product.create(productCreateRequest);
+        for (ProductVariantCreateRequest variantCreateRequest : productVariantCreateRequest) {
+            product.addVariant(ProductVariant.create(variantCreateRequest));
+        }
+        return product;
+    }
+
+  public static Product createWithoutVariant() {
+    ProductCreateRequest productCreateRequest = productCreateRequest(ProductType.OPTION);
+    Product product = Product.create(productCreateRequest);
+    ProductVariant productVariant1 =
+        ProductVariant.create(new ProductVariantCreateRequest(ProductColor.RED, ProductSize.M));
+    ProductVariant productVariant2 =
+        ProductVariant.create(new ProductVariantCreateRequest(ProductColor.BLACK, ProductSize.M));
+    product.addVariant(productVariant1);
+    product.addVariant(productVariant2);
+
+    return product;
+  }
+
+  public static Product createWithVariant() {
+    ProductCreateRequest productCreateRequest = productCreateRequest(ProductType.SIMPLE);
+    return Product.create(productCreateRequest);
+  }
+}
